3a1b2dda7e4280f45f241b02af3c0901b0d444d4
Kasia Zukowska <kzukowska@hotmail.com>
2018-05-12 16:42:55 +0200

    Not finished bubbles

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ d3.json("districtsOfSF.json", function(json) {
          });
          if (d.properties.name) {
            tooltip.classed('hidden', false)
-             .attr('style', 'left:' + (mouse[0] + 640) + 'px; top:' + (mouse[1] + 20) + 'px')
+             .attr('style', 'left:' + (mouse[0] + 390) + 'px; top:' + (mouse[1] + 150) + 'px')
              .html("<p class=\"centerTip\">" + d.properties.name + "</p>");
          };
        })
@@ -106,117 +106,6 @@ d3.json("districtsOfSF.json", function(json) {
 
     d3.csv("./filmLocationsInSF.csv", function(data) {
 
-      var width = 1200, height = 1000;
-      var fill = d3.scale.ordinal().range(['#f4fc83','#54daf2'])
-
-      var svg = d3.select(".chart").append("svg")
-         .attr("width", width)
-         .attr("height", height);
-
-     for (var j = 0; j < data.length; j++) {
-       data[j].radius =+ 3;
-       data[j].x = Math.random() * width;
-       data[j].y = Math.random() * height;
-     }
-
-     var padding = 2;
-     var maxRadius = d3.max(_.pluck(data, 'radius'));
-
-     var getCenters = function (vname, size) {
-       var centers, map;
-       centers = _.uniq(_.pluck(data, vname)).map(function (d) {
-         return {name: d, value: 1};
-       });
-       console.log(centers)
-       map = d3.layout.treemap().size([width, height]);
-       map.nodes({children: centers});
-       return centers;
-     };
-
-     var nodes = svg.selectAll("circle")
-          .data(data);
-
-        nodes.enter().append("circle")
-          .attr("class", "node")
-          .attr("cx", function (d) { return d.x; })
-          .attr("cy", function (d) { return d.y; })
-          .attr("r", function (d) { return d.radius; })
-          .style("fill", function (d) { return fill(d.Director); })
-
-        var force = d3.layout.force();
-
-        draw('Production_Company');
-
-        $( ".btn" ).click(function() {
-          draw(this.id);
-        });
-
-        function draw (varname) {
-          var centers = getCenters(varname, [800, 800]);
-          force.on("tick", tick(centers, varname));
-          labels(centers)
-          force.start();
-        }
-
-        function tick (centers, varname) {
-          var foci = {};
-          for (var i = 0; i < centers.length; i++) {
-            foci[centers[i].name] = centers[i];
-          }
-          return function (e) {
-            for (var i = 0; i < data.length; i++) {
-              var o = data[i];
-              var f = foci[o[varname]];
-              o.y += ((f.y + (f.dy / 2)) - o.y) * e.alpha;
-              o.x += ((f.x + (f.dx / 2)) - o.x) * e.alpha;
-            }
-            nodes.each(collide(.11))
-              .attr("cx", function (d) { return d.x; })
-              .attr("cy", function (d) { return d.y; });
-          }
-        }
-
-        function labels (centers) {
-          svg.selectAll(".label").remove();
-
-          svg.selectAll(".label")
-          .data(centers).enter().append("text")
-          .attr("class", "label")
-          .text(function (d) { return d.name })
-          .attr("transform", function (d) {
-            return "translate(" + (d.x + (d.dx / 2)) + ", " + (d.y + 20) + ")";
-          });
-        }
-
-
-        function collide(alpha) {
-          var quadtree = d3.geom.quadtree(data);
-          return function (d) {
-            var r = d.radius + maxRadius + padding,
-                nx1 = d.x - r,
-                nx2 = d.x + r,
-                ny1 = d.y - r,
-                ny2 = d.y + r;
-            quadtree.visit(function(quad, x1, y1, x2, y2) {
-              if (quad.point && (quad.point !== d)) {
-                var x = d.x - quad.point.x,
-                    y = d.y - quad.point.y,
-                    l = Math.sqrt(x * x + y * y)
-                    r = d.radius + quad.point.radius + padding;
-                if (l < r) {
-                  l = (l - r) / l * alpha;
-                  d.x -= x *= l;
-                  d.y -= y *= l;
-                  quad.point.x += x;
-                  quad.point.y += y;
-                }
-              }
-              return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-            });
-          };
-        }
-
-
       g.selectAll("circle")
         .data(data)
         .enter()
@@ -238,6 +127,147 @@ d3.json("districtsOfSF.json", function(json) {
         .style("position", "absolute")
     })
 })
+
+changeYear('2005-2018');
+//
+d3.select('#decades').on("change", function () {
+  var sect = document.getElementById("decades");
+	var section = sect.options[sect.selectedIndex].value;
+  changeYear(section);
+});
+
+
+var datasource;
+var backUp=null;
+
+function changeYear(year){
+  displayDots(year)
+}
+
+function displayDots(year){
+  var from = year.substr(0,year.indexOf('-'));
+  var to = year.substr(year.indexOf('-')+1,year.length);
+
+  // svg.remove()
+  d3.csv('filmLocationsInSF.csv', function(data) {
+    data = data.filter((d)=>{
+      return (d.Release_Year >= from && d.Release_Year <= to);
+    });
+
+    var width = 800, height = 600;
+    var fill = d3.scale.ordinal().range(['#f4fc83','#54daf2'])
+
+    var svg = d3.select(".chart").append("svg")
+       .attr("width", width)
+       .attr("height", height);
+
+   for (var j = 0; j < data.length; j++) {
+     data[j].radius =+ 3;
+     data[j].x = Math.random() * width;
+     data[j].y = Math.random() * height;
+   }
+
+   var padding = 2;
+   var maxRadius = d3.max(_.pluck(data, 'radius'));
+
+   var getCenters = function (vname, size) {
+     var centers, map;
+     centers = _.uniq(_.pluck(data, vname)).map(function (d) {
+       return {name: d, value: 1};
+     });
+     map = d3.layout.treemap().size([width, height / 2]);
+     map.nodes({children: centers});
+     return centers;
+   };
+
+   if (nodes) {
+     nodes.exit().remove();
+   }
+   var nodes = svg.selectAll("circle")
+        .data(data);
+
+      nodes.enter().append("circle")
+        .attr("class", "node")
+        .attr("cx", function (d) { return d.x; })
+        .attr("cy", function (d) { return d.y; })
+        .attr("r", function (d) { return d.radius; })
+        .style("fill", function (d) { return fill(d.Director); })
+
+      var force = d3.layout.force();
+
+      draw('Production_Company');
+
+      $( ".btn" ).click(function() {
+        draw(this.id);
+      });
+
+      function draw (varname) {
+        var centers = getCenters(varname, [400, 400]);
+        force.on("tick", tick(centers, varname));
+        labels(centers)
+        force.start();
+      }
+
+      function tick (centers, varname) {
+        var foci = {};
+        for (var i = 0; i < centers.length; i++) {
+          foci[centers[i].name] = centers[i];
+        }
+        return function (e) {
+          for (var i = 0; i < data.length; i++) {
+            var o = data[i];
+            var f = foci[o[varname]];
+            o.y += ((f.y + (f.dy / 2)) - o.y) * e.alpha;
+            o.x += ((f.x + (f.dx / 2)) - o.x) * e.alpha;
+          }
+          nodes.each(collide(.11))
+            .attr("cx", function (d) { return d.x; })
+            .attr("cy", function (d) { return d.y; });
+        }
+      }
+
+
+      function labels (centers) {
+        svg.selectAll(".label").remove();
+
+        svg.selectAll(".label")
+        .data(centers).enter().append("text")
+        .attr("class", "label")
+        .text(function (d) { return d.name })
+        .attr("transform", function (d) {
+          return "translate(" + (d.x + (d.dx / 3)) + ", " + (d.y + 20) + ")";
+        });
+      }
+
+
+      function collide(alpha) {
+        var quadtree = d3.geom.quadtree(data);
+        return function (d) {
+          var r = d.radius + maxRadius + padding,
+              nx1 = d.x - r,
+              nx2 = d.x + r,
+              ny1 = d.y - r,
+              ny2 = d.y + r;
+          quadtree.visit(function(quad, x1, y1, x2, y2) {
+            if (quad.point && (quad.point !== d)) {
+              var x = d.x - quad.point.x,
+                  y = d.y - quad.point.y,
+                  l = Math.sqrt(x * x + y * y)
+                  r = d.radius + quad.point.radius + padding;
+              if (l < r) {
+                l = (l - r) / l * alpha;
+                d.x -= x *= l;
+                d.y -= y *= l;
+                quad.point.x += x;
+                quad.point.y += y;
+              }
+            }
+            return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+          });
+        };
+      }
+  })
+}
 
 
 function clicked(d) {
